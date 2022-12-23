b93f78cadd5112ca9056657180bd5130400f8fe2
Volodymyr Volianyk <wrspct@gmail.com>
2022-12-23 23:17:00 +0200

    Initial commit

--- src/main/java/com/fly/project/controller/MainClass.java
+++ src/main/java/com/fly/project/controller/MainClass.java
@@ -10,7 +10,9 @@ import com.fly.project.service.FlightService;
 import com.fly.project.service.ReservationService;
 import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -22,13 +24,19 @@ public class MainClass {
         FlightService flightService = new FlightService(flightScheduleRepository);
         TicketRepository ticketRepository = new TicketRepository();
         ReservationService reservationService = new ReservationService(ticketRepository);
+// input data
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome! Please enter departure airport:");
+        String departure=br.readLine();
+        System.out.println("Please enter arrived airport:");
+        String arrived=br.readLine();
 
-        List<Flight> resultSet = flightService.searchFlight("Lytsk", "Lviv", LocalDate.of(2022, 12, 31));
+        List<Flight> resultSet = flightService.searchFlight(departure, arrived, LocalDate.of(2022, 12, 31));
         List<Ticket> tickets = reservationService.makeReservation(resultSet.get(0), List.of(Adult.builder().fullName(new Person.FullName("a", "b")).build()));
         for (Flight flight : resultSet) {
             System.out.println(flight);
         }
-        for (Ticket ticket: tickets){
+        for (Ticket ticket : tickets) {
             System.out.println(ticket);
         }
 
